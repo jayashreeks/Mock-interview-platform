1971ea078b912e9f442a8c81090440a98775da6a
Jayashree K S <jayashree.ks@tsgforce.com>
2025-05-11 20:12:45 +0530

    trying to check logs for route.ts in prod

--- app/api/vapi/generate/route.ts
+++ app/api/vapi/generate/route.ts
@@ -7,6 +7,8 @@ export async function GET(){
     return Response.json({ success: true, data : 'Thank you!' },{ status: 200 });
 }
 
+export const dynamic = 'force-dynamic';
+
 export async function POST(request: Request){
     const { type, role, level, techstack, amount, userid } = await request.json();
     try{
